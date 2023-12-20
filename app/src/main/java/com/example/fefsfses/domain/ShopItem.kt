4aaa942bc5bfa809ea60bebfea13d0bcc698f8c4
Barakat <adam1baranov@gmail.com>
2023-12-20 21:11:30 +0300

    c

---
+++ app/src/main/java/com/example/fefsfses/domain/ShopItem.kt
@@ -0,0 +1,8 @@
+package com.example.fefsfses.domain
+
+data class ShopItem(
+    val count:Int? = 0,
+    val name:String? = "",
+    val id:Int ? = -1,
+    val enought:Boolean? = true
+)
---
+++ app/src/main/java/com/example/fefsfses/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fefsfses.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fefsfses.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
